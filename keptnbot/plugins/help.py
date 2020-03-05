f78120a90e84ed84ce1bd0b8fdf51ad90cb6329e
Zohaib <zuh1@hotmail.com>
2020-03-05 19:16:40 +0400

    updated help command

--- keptnbot/plugins/help.py
+++ keptnbot/plugins/help.py
@@ -7,7 +7,6 @@ import json
 @respond_to('help$', re.IGNORECASE)
 def hello_reply(message):
     reply = []
-    reply.append(u'•`{0}`'.format('@<Botname> start-evaluation <project> <service> <stage> <start> <end> | Send start-evaluation event. <start> & <end> date time should be in ISO format (2019-12-13T13:00)'))
     reply.append(u'•`{0}`'.format('@<Botname> start-evaluation <project> <service> <stage> <minutes> | example: @Botname start-evaluation <project> <service> <stage> 10'))
     reply.append(u'•`{0}`'.format('@<Botname> start-evaluation <project> <service> <stage> <start-time> <end-time> | example: @Botname start-evaluation <project> <service> <stage> 12:00 12:15'))
     reply.append(u'•`{0}`'.format('@<Botname> start-evaluation <project> <service> <stage> <date> <start-time> <end-time> | example: @Botname start-evaluation <project> <service> <stage> 30/01/2020 12:00 12:15'))
