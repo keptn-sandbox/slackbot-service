d0c29314aea1caa8f31c5c8ad9ccff54742df639
Zohaib <zuh1@hotmail.com>
2020-07-02 11:15:36 +0400

    updated help

--- keptnbot/plugins/help.py
+++ keptnbot/plugins/help.py
@@ -10,6 +10,9 @@ def help_reply(message):
     reply.append(u'•`{0}`'.format('@<Botname> start-evaluation <project> <service> <stage> <minutes> | example: @Botname start-evaluation <project> <service> <stage> 10'))
     reply.append(u'•`{0}`'.format('@<Botname> start-evaluation <project> <service> <stage> <start-time> <end-time> | example: @Botname start-evaluation <project> <service> <stage> 12:00 12:15'))
     reply.append(u'•`{0}`'.format('@<Botname> start-evaluation <project> <service> <stage> <date> <start-time> <end-time> | example: @Botname start-evaluation <project> <service> <stage> 30/01/2020 12:00 12:15'))
-
     reply.append(u'•`{0}`'.format('@<Botname> deployment-finished <project> <service> <stage> <test-strategy> <deployment-url> | example: @Botname deployment-finished myproject myservice myproduction myteststrategy https://myservice.myproject.com/'))
+    reply.append(u'•`{0}`'.format('@<Botname> get projects | returns projects running in keptn'))
+    reply.append(u'•`{0}`'.format('@<Botname> get projects | returns projects running in keptn'))
+    reply.append(u'•`{0}`'.format('@<Botname> get services of <project> | returns services running under a project'))
+    
     message.send(u'\n'.join(reply))
